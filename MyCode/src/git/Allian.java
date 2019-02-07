bcab19eaa373f0e9cd0e98974d0c8d973208234b
aaquib <aki.mohd81@gmail.com>
2019-02-07 19:15:43 +0530

    first commit uploading all files

---
+++ MyCode/src/git/Allian.java
@@ -0,0 +1,17 @@
+package git;
+
+public class Allian {
+
+	int n;
+	int m;
+	int sum=0;
+	int multi=1;
+	int add(int n, int m)
+	{
+		return sum=n+m;
+	}
+	double mul(int n, int m)
+	{
+	return	multi=n*m;
+	}
+}
---
+++ MyCode/src/git/Calculate.java
@@ -0,0 +1,14 @@
+package git;
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Allian a=new Allian();
+ int s=a.add(5,10);
+	double d=a.mul(5, 10);
+		System.out.println(s+"  "+d);
+	}
+
+}
